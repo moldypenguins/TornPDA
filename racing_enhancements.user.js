7db4cbd29ed5d22d1554c0bc573e09d7219c5da1
Craig Roberts <craig.roberts@crdevel.com>
2024-01-17 17:47:20 -0500

    Update racing_enhancements.user.js

--- racing_enhancements.user.js
+++ racing_enhancements.user.js
@@ -2,7 +2,7 @@
 // @name         Torn PDA - Racing Enhancements
 // @namespace    TornPDA.racing_enhancements
 // @version      0.3.0
-// @description  Show car's current speed, precise skill, official race penalty, racing skill of others.
+// @description  Show racing skill, current speed, race results, precise skill.
 // @author       moldypenguins [2881784] - Adapted from Lugburz
 // @match        https://www.torn.com/loader.php?sid=racing*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
@@ -63,7 +63,7 @@
                       '<ul id="racingEnhancementsContainer" style="display: none;">' +
                           '<li><input type="checkbox" id="showSkillChk"><label>Show racing skill</label></li>' +
                           '<li><input type="checkbox" id="showSpeedChk"><label>Show current speed</label></li>' +
-                          '<li><input type="checkbox" id="showResultsChk"><label>Show results</label></li>' +
+                          '<li><input type="checkbox" id="showResultsChk"><label>Show race results</label></li>' +
                       '</ul>' + 
                   '</div>';
       $('.drivers-list').children(":first").after(div);
@@ -92,7 +92,7 @@
       if (!driverIds || !driverIds.length) { return; }
 
       updating = true;
-      $('#updating').size() < 1 && $('#racingEnhancements').prepend('<span id="updating"></span>');
+      $('#updating').size() < 1 && $('#racingEnhancementsTitle').before('<span id="updating"></span>');
 
       const racingSkills = await getRacingSkillForDrivers(driverIds);
       for (let driver of leaderboard.querySelectorAll('.driver-item')) {
@@ -157,18 +157,159 @@
               maybeClear();
               return;
           }
+          if($('#racingdetails').find('li.pd-completion').text().indexOf('%') >= 0) {
+              let laps = $('#racingupdatesnew').find('div.title-black').text().split(" - ")[1].split(" ")[0];
+              let len = $('#racingupdatesnew').find('div.track-info').attr('data-length').replace('mi', '');
+              let compl = $('#racingdetails').find('li.pd-completion').text().replace('%', '');
 
-          let laps = $('#racingupdatesnew').find('div.title-black').text().split(" - ")[1].split(" ")[0];
-          let len = $('#racingupdatesnew').find('div.track-info').attr('data-length').replace('mi', '');
-          let compl = $('#racingdetails').find('li.pd-completion').text().replace('%', '');
-
-          if (last_compl >= 0) {
-              let speed = (compl - last_compl) / 100 * laps * len * 60 * 60 * 1000 / period;
-              $('#speed_mph').text(typeof(speed) === 'number' ? speed.toFixed(2) + 'mph' : speed);
+              if (last_compl >= 0) {
+                  let speed = (compl - last_compl) / 100 * laps * len * 60 * 60 * 1000 / period;
+                  $('#speed_mph').text(speed.toFixed(2) + 'mph');
+              }
+              last_compl = compl;
+          } else {
+              $('#speed_mph').text('0.00mph');
           }
-          last_compl = compl;
       }, period);
   }
+
+
+
+
+  async function updateSkill(level) {
+      const skill = Number(level).toFixed(5);
+      const prev = GM.getValue('racinglevel');
+  
+      const now = Date.now();
+      if (prev !== "undefined" && typeof prev !== "undefined" && level > prev) {
+          const lastInc = Number(level - prev).toFixed(5);
+          if (lastInc) {
+              $('div.skill').append(`<div style="margin-top:10px;">Last gain: ${lastInc}</div>`);
+          }
+      }
+      GM.setValue('racinglevel', level);
+  
+      if ($('#racingMainContainer').find('div.skill').size() > 0) {
+          if ($("#sidebarroot").find("a[class^='menu-value']").size() > 0) {
+              // move the elements to the left a little bit to fit 5th decimal digit in desktop mode
+              $('#racingMainContainer').find('div.skill-desc').css('left', '5px');
+              $('#racingMainContainer').find('div.skill').css('left', '5px').text(skill);
+          } else {
+              $('#racingMainContainer').find('div.skill').text(skill);
+          }
+      }
+  }
+  
+
+
+  async function parseRacingData(data) {
+      await updateSkill(data.user.racinglevel);
+
+      // display race link
+      if ($('#raceLink').size() < 1) {
+          RACE_ID = data.raceID;
+          const raceLink = `<a id="raceLink" href="https://www.torn.com/loader.php?sid=racing&tab=log&raceID=${RACE_ID}">` + 
+          '<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12"><g><path d="M3.09,4.36c1.25-1.25,3.28-1.26,4.54,0,.15.15.29.32.41.5l-1.12,1.12c-.32-.74-1.13-1.15-1.92-.97-.31.07-.59.22-.82.45l-2.15,2.15c-.65.66-.63,1.72.03,2.37.65.63,1.69.63,2.34,0l.66-.66c.6.24,1.25.34,1.89.29l-1.47,1.47c-1.26,1.26-3.29,1.26-4.55,0-1.26-1.26-1.26-3.29,0-4.55h0l2.15-2.15ZM6.51.94l-1.47,1.46c.64-.05,1.29.05,1.89.29l.66-.66c.65-.65,1.72-.65,2.37,0,.65.65.65,1.72,0,2.37h0l-2.15,2.15c-.66.65-1.71.65-2.37,0-.15-.15-.28-.33-.36-.53l-1.12,1.12c.12.18.25.34.4.49,1.25,1.26,3.29,1.26,4.54,0,0,0,0,0,0,0l2.15-2.15c1.26-1.26,1.25-3.29,0-4.55-1.26-1.26-3.29-1.25-4.55,0Z" fill="currentColor" stroke-width="0"></path></g></svg>' + 
+          '</a>';
+          $('#racingEnhancements').prepend(raceLink);
+          $('#raceLink').on('click', function(event) {
+              event.preventDefault();
+              event.stopPropagation();
+              //TODO: add tooltip
+              //SEE: https://www.torn.com/page.php?sid=education&category=7&course=67
+              GM.setClipboard(`https://www.torn.com/loader.php?sid=racing&tab=log&raceID=${RACE_ID}`);
+          });
+      }
+
+      // calc, sort & show race results
+      if (SHOW_RESULTS && data.timeData.status >= 3) {
+          const carsData = data.raceData.cars;
+          const carInfo = data.raceData.carInfo;
+          const trackIntervals = data.raceData.trackData.intervals.length;
+          let results = [], crashes = [];
+  
+          for (const playername in carsData) {
+              
+              const userId = carInfo[playername].userID;
+              const intervals = atob(carsData[playername]).split(',');
+              let raceTime = 0;
+              let bestLap = 9999999999;
+              if (intervals.length / trackIntervals == data.laps) {
+                  for (let i = 0; i < data.laps; i++) {
+                      let lapTime = 0;
+                      for (let j = 0; j < trackIntervals; j++) {
+                          lapTime += Number(intervals[i * trackIntervals + j]);
+                      }
+                      bestLap = Math.min(bestLap, lapTime);
+                      raceTime += Number(lapTime);
+                  }
+                  results.push([playername, userId, raceTime, bestLap]);
+              } else {
+                  crashes.push([playername, userId, 'crashed']);
+              }
+          }
+          
+          // sort by time
+          results.sort(compare);
+          //addExportButton(results, crashes, data.user.playername, data.raceID, data.timeData.timeEnded);
+
+          showResults(results);
+          showResults(crashes, results.length);
+      }
+  }
+  
+  // compare by time
+  function compare(a, b) {
+      if (a[2] > b[2]) return 1;
+      if (b[2] > a[2]) return -1;
+  
+      return 0;
+  }
+
+  function showResults(results, start = 0) {
+      for (let i = 0; i < results.length; i++) {
+          $('#leaderBoard').children('li').each(function() {
+              const name = $(this).find('li.name').text().trim();
+              const status = $(this).find('li.status-wrap');
+              if (name == results[i][0]) {
+                  const p = i + start + 1;
+
+                  if (p == 1) {
+                      status.html('<div class="status gold"></div>');
+                  } else if (p == 2) {
+                      status.html('<div class="status silver"></div>');
+                  } else if (p == 3) {
+                      status.html('<div class="status bronze"></div>');
+                  } else if (p >= 4) {
+                      status.html(`<div class="finished-${p} finished"></div>`);
+                  } else  {
+                      status.html(`<div class="status racing"></div>`);
+                  }
+              
+                  /*
+                  const position = p === 1 ? 'gold' : (p === 2 ? 'silver' : (p === 3 ? 'bronze' : ''));
+                  let place;
+                  if (p != 11 && (p%10) == 1)
+                      place = p + 'st';
+                  else if (p != 12 && (p%10) == 2)
+                      place = p + 'nd';
+                  else if (p != 13 && (p%10) == 3)
+                      place = p + 'rd';
+                  else
+                      place = p + 'th';
+  
+                  const result = typeof results[i][2] === 'number' ? formatTimeMsec(results[i][2] * 1000) : results[i][2];
+                  const bestLap = results[i][3] ? formatTimeMsec(results[i][3] * 1000) : null;
+                  $(this).find('li.name').html($(this).find('li.name').html().replace(name, ((SHOW_POSITION_ICONS && position) ? `<i class="race_position ${position}"></i>` : '') + `${name} ${place} ${result}` + (bestLap ? ` (best: ${bestLap})` : '')));
+                  */
+                  return false;
+              }
+          });
+      }
+  }
+
+
+
 
 
 
@@ -246,118 +387,6 @@
 
 
   
-  async function updateSkill(level) {
-      const skill = Number(level).toFixed(5);
-      const prev = GM.getValue('racinglevel');
-  
-      const now = Date.now();
-      if (prev !== "undefined" && typeof prev !== "undefined" && level > prev) {
-          const lastInc = Number(level - prev).toFixed(5);
-          if (lastInc) {
-              $('div.skill').append(`<div style="margin-top:10px;">Last gain: ${lastInc}</div>`);
-          }
-      }
-      GM.setValue('racinglevel', level);
-  
-      if ($('#racingMainContainer').find('div.skill').size() > 0) {
-          if ($("#sidebarroot").find("a[class^='menu-value']").size() > 0) {
-              // move the elements to the left a little bit to fit 5th decimal digit in desktop mode
-              $('#racingMainContainer').find('div.skill-desc').css('left', '5px');
-              $('#racingMainContainer').find('div.skill').css('left', '5px').text(skill);
-          } else {
-              $('#racingMainContainer').find('div.skill').text(skill);
-          }
-      }
-  }
-  
-
-
-  async function parseRacingData(data) {
-      await updateSkill(data.user.racinglevel);
-
-      // display race link
-      if ($('#raceLink').size() < 1) {
-          RACE_ID = data.raceID;
-          const raceLink = `<a id="raceLink" href="https://www.torn.com/loader.php?sid=racing&tab=log&raceID=${RACE_ID}" style="float: right; margin-left: 12px;">Link to the race</a>`;
-          $(raceLink).insertAfter('#racingEnhSettings');
-      }
-
-      //GM.log(`[Racing Enhancements] STATUS: ${data.timeData.status}`);
-  
-      // calc, sort & show race results
-      if (data.timeData.status >= 3) {
-          const carsData = data.raceData.cars;
-          const carInfo = data.raceData.carInfo;
-          const trackIntervals = data.raceData.trackData.intervals.length;
-          let results = [], crashes = [];
-  
-          for (const playername in carsData) {
-              const userId = carInfo[playername].userID;
-              const intervals = decode64(carsData[playername]).split(',');
-              let raceTime = 0;
-              let bestLap = 9999999999;
-  
-              if (intervals.length / trackIntervals == data.laps) {
-                  for (let i = 0; i < data.laps; i++) {
-                      let lapTime = 0;
-                      for (let j = 0; j < trackIntervals; j++) {
-                          lapTime += Number(intervals[i * trackIntervals + j]);
-                      }
-                      bestLap = Math.min(bestLap, lapTime);
-                      raceTime += Number(lapTime);
-                  }
-                  results.push([playername, userId, raceTime, bestLap]);
-              } else {
-                  crashes.push([playername, userId, 'crashed']);
-              }
-          }
-  
-          // sort by time
-          results.sort(compare);
-          //addExportButton(results, crashes, data.user.playername, data.raceID, data.timeData.timeEnded);
-
-          if (SHOW_RESULTS) {
-              showResults(results);
-              showResults(crashes, results.length);
-          }
-      }
-  }
-  
-  // compare by time
-  function compare(a, b) {
-      if (a[2] > b[2]) return 1;
-      if (b[2] > a[2]) return -1;
-  
-      return 0;
-  }
-
-  function showResults(results, start = 0) {
-      for (let i = 0; i < results.length; i++) {
-          $('#leaderBoard').children('li').each(function() {
-              const name = $(this).find('li.name').text().trim();
-              const status = $(this).find('li.status-wrap');
-              if (name == results[i][0]) {
-                const p = i + start + 1;
-
-                switch(p) {
-                    case 1:
-                        status.html('<div class="status gold"></div>');
-                        break;
-                    case 2:
-                        status.html('<div class="status silver"></div>');
-                        break;
-                    case 3:
-                        status.html('<div class="status bronze"></div>');
-                        break;
-                    default:
-                        status.html(`<div class="finished-${p} finished"></div>`);
-                        break;
-                }
-                return false;
-            }
-          });
-      }
-  }
 
 
 
@@ -371,6 +400,10 @@
       padding:5px 10px;
       margin-bottom:2px;
       background:repeating-linear-gradient(90deg,#242424,#242424 2px,#2e2e2e 0,#2e2e2e 4px);
+  }
+  #raceLink{
+      display:inline-block;
+      float:right;
   }
   #racingEnhancementsTitle {
       text-decoration:none;
@@ -400,6 +433,7 @@
       height: 10px;
       display: inline-block;
       float: right;
+      margin-right:10px;
   }
   #error { color:red; font-size:10px; float:right; }
   `);
