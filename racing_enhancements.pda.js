065e1ff0ae3c6597438b9f39e563a31f41d9a571
Craig Roberts <craig.roberts@crdevel.com>
2024-01-19 22:34:43 -0500

    Update racing_enhancements.pda.js

--- racing_enhancements.pda.js
+++ racing_enhancements.pda.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn PDA - Racing Enhancements
 // @namespace    TornPDA.racing_enhancements
-// @version      0.4.6
+// @version      0.4.7
 // @description  Show racing skill, current speed, race results, precise skill.
 // @author       moldypenguins [2881784] - Adapted from Lugburz
 // @match        https://www.torn.com/loader.php?sid=racing*
@@ -211,16 +211,21 @@
       if ($("#raceLinkPlaceholder").length > 0) {
         $("#raceLinkPlaceholder").remove();
       }
-      let raceLink = `<a id="raceLink" href="https://www.torn.com/loader.php?sid=racing&tab=log&raceID=${data.raceID}">` +
+      let raceLink = `<a id="raceLink" title="Copy link" href="https://www.torn.com/loader.php?sid=racing&tab=log&raceID=${data.raceID}">` +
           '<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12"><g><path d="M3.09,4.36c1.25-1.25,3.28-1.26,4.54,0,.15.15.29.32.41.5l-1.12,1.12c-.32-.74-1.13-1.15-1.92-.97-.31.07-.59.22-.82.45l-2.15,2.15c-.65.66-.63,1.72.03,2.37.65.63,1.69.63,2.34,0l.66-.66c.6.24,1.25.34,1.89.29l-1.47,1.47c-1.26,1.26-3.29,1.26-4.55,0-1.26-1.26-1.26-3.29,0-4.55h0l2.15-2.15ZM6.51.94l-1.47,1.46c.64-.05,1.29.05,1.89.29l.66-.66c.65-.65,1.72-.65,2.37,0,.65.65.65,1.72,0,2.37h0l-2.15,2.15c-.66.65-1.71.65-2.37,0-.15-.15-.28-.33-.36-.53l-1.12,1.12c.12.18.25.34.4.49,1.25,1.26,3.29,1.26,4.54,0,0,0,0,0,0,0l2.15-2.15c1.26-1.26,1.25-3.29,0-4.55-1.26-1.26-3.29-1.25-4.55,0Z" fill="currentColor" stroke-width="0"></path></g></svg>' +
         "</a>";
       $("#racingEnhancements").prepend(raceLink);
       $("#raceLink").on("click", function (event) {
         event.preventDefault();
         event.stopPropagation();
-        //TODO: add tooltip
-        //SEE: https://www.torn.com/page.php?sid=education&category=7&course=67
         GM.setClipboard(`https://www.torn.com/loader.php?sid=racing&tab=log&raceID=${data.raceID}`);
+
+        let tooltip = $(`#${$("#raceLink").attr("aria-describedby")} .ui-tooltip-content`);
+        if(tooltip.length > 0) {
+          tooltip[0].childNodes[0].nodeValue = 'Copied';
+          let newLeft = (Number(tooltip.parent('div').css('left').replace('px', '')) + 6) + 'px';
+          tooltip.parent('div').css("left", newLeft);
+        }
       });
     }
 
