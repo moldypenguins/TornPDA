0783500edc4e57a0ca31ab105405f3a28de0b718
Craig Roberts <craig.roberts@crdevel.com>
2025-07-12 13:50:55 -0400

    Update RacingPlus.user.js

--- RacingPlus.user.js
+++ RacingPlus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn PDA - Racing+
 // @namespace    TornPDA.RacingPlus
-// @version      0.5
+// @version      0.6
 // @description  Show racing skill, current speed, race results, precise skill, upgrade parts.
 // @author       moldypenguins [2881784] - Adapted from Lugburz [2386297]
 // @match        https://www.torn.com/loader.php?sid=racing*
@@ -242,6 +242,11 @@
 
   const addRacingPlusStyles = async () => {
     console.log('Racing+: Adding styles...');
+
+    if (!GM_addStyle) {
+      GM_addStyle = GM.addStyle;
+    }
+
     // Add styles
     GM_addStyle(`
       div.racing-plus-window {
