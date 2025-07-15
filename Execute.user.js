2569fb2b73655b51ac97c0ed74eef345a7a52c35
Craig Roberts <craig.roberts@crdevel.com>
2025-07-15 07:14:22 -0400

    Update Execute.user.js

--- Execute.user.js
+++ Execute.user.js
@@ -1,27 +1,35 @@
 // ==UserScript==
 // @name         Execute
 // @namespace    TornPDA.Execute
-// @version      0.1
+// @version      0.2
+// @license      MIT
 // @description  Shows execute limit in health bar.
 // @author       moldypenguins [2881784]
 // @match        https://www.torn.com/loader.php?sid=attack*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
 // @updateURL    https://github.com/moldypenguins/TornPDA/raw/main/execute.user.js
 // @downloadURL  https://github.com/moldypenguins/TornPDA/raw/main/execute.user.js
-// @grant        GM_addStyle
-// @run-at       document-body
+// @run-at       document-end
 // ==/UserScript==
 
 (function () {
   'use strict';
-
-  let executeLevel = 15;
-
+  const executeLevel = 15;
+  const PDA = {
+    addStyle: (style) => {
+      if (!style) {
+        return;
+      }
+      const s = document.createElement('style');
+      s.innerHTML = style;
+      document.head.appendChild(s);
+    },
+  };
   let healthBar = $('div [aria-label^="Progress:"]');
   if (healthBar) {
     let targetHealth = parseFloat(healthBar.attr('aria-label').replace(/Progress: (\d{1,3}\.?\d{0,2})%/, '$1'));
     if (targetHealth <= executeLevel) {
-      GM_addStyle(`
+      PDA.addStyle(`
         .progress___iG5el {
           background-image: linear-gradient(#FFB46C, #FFA737) !important;
         }
