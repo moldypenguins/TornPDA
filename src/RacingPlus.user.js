fe8b85b7bbcfcbc50ce416ac34dd0e9361645a16
Craig Roberts <craig.roberts@crdevel.com>
2025-12-19 22:46:11 -0500

    Update RacingPlus.user.js

--- src/RacingPlus.user.js
+++ src/RacingPlus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TornPDA-Racing+
 // @namespace    TornPDA.RacingPlus
-// @version      0.99.10
+// @version      0.99.11
 // @license      MIT
 // @description  Show racing skill, current speed, race results, precise skill, upgrade parts.
 // @author       moldypenguins [2881784] - Adapted from Lugburz [2386297] - With flavours from TheProgrammer [2782979]
